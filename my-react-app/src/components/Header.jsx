3326528132067f372677260871a6223a8bd3b832
Your Name <adujulio@example.com>
2026-05-21 01:34:34 +0100

    “”

--- my-react-app/src/components/Header.jsx
+++ my-react-app/src/components/Header.jsx
@@ -8,3 +8,77 @@ function Header(){
     )
 }
 export default Header
+
+// length
+// function tunggle(){
+//     iscontact(prevcontact => ({
+//         ...prevcontact
+//         isfav: !prevcontact.isfav
+//     }))
+// }
+
+
+
+   // const [isShown, setisShown]= useState(false)
+    
+    
+    // function toogle(){
+    //     setisShown(previsShown=>({
+    //         previsShown,
+    //         isShown: !isShown.isShown
+    //     }))
+            
+    // }
+    // console.log(isShown)
+
+
+//     import React from "react"
+
+// export default function App() {
+//     const [messages, setMessages] = React.useState(["a", "b"])
+//     /**
+//      * Challenge:
+//      * - If there are no unread messages, display "You're all caught up!"
+//      * - If there's exactly 1 unread message, it should read "You have 
+//      *   1 unread message" (singular)
+//      * - If there are > 1 unread messages, display "You have <n> unread
+//      *   messages" (plural)
+//      */
+    
+//     // function toggle(){
+//     //     setMessages(prevmessages=>({
+//     //         messages:!messages
+//     //     }))
+//     // }
+    
+//     function myMessages(){
+//         if(messages.length === 0){
+//             return <h1>"You're all caught up!"</h1>
+//         }
+//         else if(messages.length === 1){
+//           return <h1>"You have 1 unread message"</h1>
+//         }
+//         else if(messages.length > 1){
+//            return <h1>"You have {messages.length} unread messages"</h1>
+//         }
+//     } 
+
+//     return (
+//         <div>
+//             <h1>{myMessages()}</h1>
+//         </div>
+//     )
+    
+// }
+
+// import pads from "./pads"
+// import React from "react"
+
+// export default function App(props) {
+//     const [usePads, setusePads] = React.useState([]);
+    
+//    const ingridentItems = pads.map(pad =>(
+//             <li key={pad}>
+//                 color={props.color}
+//             </li>
+//     ))
